a17769e2d3e9c5c51d4f0a0689e1292c83a3d92f
Thomas Wittrup Fischer <thofisch@gmail.com>
2022-03-20 21:07:23 +0100

    remove old json deserializer

--- src/Dafda.Tests/Builders/JsonIncomingMessageFactoryBuilder.cs
+++
@@ -1,9 +0,0 @@
-﻿using Dafda.Consuming;
-
-namespace Dafda.Tests.Builders
-{
-    internal class JsonIncomingMessageFactoryBuilder
-    {
-        public JsonIncomingMessageFactory Build() => new JsonIncomingMessageFactory();
-    }
-}
